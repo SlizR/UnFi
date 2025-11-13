b23648ab0ac8b79ca8406f7c6e6458e7690db46e
Ye-A! <markd.voznyuk@gmail.com>
2025-11-13 21:51:47 +0200

    Add UnFi boot files

---
+++ UnFi/ESP/ESP8266/NodeMCU/boot.ino
@@ -0,0 +1,90 @@
+//    UUUUUUUU     UUUUUUUU                 FFFFFFFFFFFFFFFFFFFFFF  iiii  
+//    U::::::U     U::::::U                 F::::::::::::::::::::F i::::i 
+//    U::::::U     U::::::U                 F::::::::::::::::::::F  iiii  
+//    UU:::::U     U:::::UU                 FF::::::FFFFFFFFF::::F        
+//     U:::::U     U:::::Unnnn  nnnnnnnn      F:::::F       FFFFFFiiiiiii 
+//     U:::::D     D:::::Un:::nn::::::::nn    F:::::F             i:::::i 
+//     U:::::D     D:::::Un::::::::::::::nn   F::::::FFFFFFFFFF    i::::i 
+//     U:::::D     D:::::Unn:::::::::::::::n  F:::::::::::::::F    i::::i 
+//     U:::::D     D:::::U  n:::::nnnn:::::n  F:::::::::::::::F    i::::i 
+//     U:::::D     D:::::U  n::::n    n::::n  F::::::FFFFFFFFFF    i::::i 
+//     U:::::D     D:::::U  n::::n    n::::n  F:::::F              i::::i 
+//     U::::::U   U::::::U  n::::n    n::::n  F:::::F              i::::i 
+//     U:::::::UUU:::::::U  n::::n    n::::nFF:::::::FF           i::::::i
+//      UU:::::::::::::UU   n::::n    n::::nF::::::::FF           i::::::i
+//        UU:::::::::UU     n::::n    n::::nF::::::::FF           i::::::i
+//          UUUUUUUUU       nnnnnn    nnnnnnFFFFFFFFFFF           iiiiiiii
+
+/*
+  UnFi Bootloader / Logger by Sliz®
+  ESP8266 NodeMCU
+  Version 1.start.up
+*/
+
+#include <Arduino.h>
+
+#define LOG Serial
+bool bootCompleted = false;
+
+#define LED_STATUS LED_BUILTIN
+
+void blinkLED(int times, int delayMs) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_STATUS, LOW);
+    delay(delayMs);
+    digitalWrite(LED_STATUS, HIGH);
+    delay(delayMs);
+  }
+}
+
+void logBootStart() {
+  LOG.println("[UnFi] Bootloader started.");
+  blinkLED(2, 200);
+}
+
+void logBootEnd() {
+  LOG.println("[UnFi] Bootloader completed.");
+  blinkLED(3, 150);
+  bootCompleted = true;
+}
+
+void logError(String msg) {
+  LOG.print("[UnFi ERROR] ");
+  LOG.println(msg);
+  blinkLED(5, 100);
+}
+
+void checkSystem() {
+  LOG.println("[UnFi] Performing system checks...");
+  blinkLED(1, 300);
+  LOG.println("[UnFi] System OK.");
+}
+
+void userSetup() {
+  // Your setup code here
+}
+
+void userLoop() {
+  // Your loop code here
+}
+
+void setup() {
+  pinMode(LED_STATUS, OUTPUT);
+  digitalWrite(LED_STATUS, HIGH);
+  Serial.begin(115200);
+
+  logBootStart();
+  checkSystem();
+
+  try { userSetup(); }
+  catch (...) { logError("Error in user setup!"); }
+
+  logBootEnd();
+}
+
+void loop() {
+  if (!bootCompleted) return;
+
+  try { userLoop(); }
+  catch (...) { logError("Error in user loop!"); }
+}
